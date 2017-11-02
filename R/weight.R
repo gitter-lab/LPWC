781ee71035d18befaded3d1df5f75a7b99739b8e
Thevaa Chandereng <chandereng@wisc.edu>
2017-11-02 02:39:18 -0500

    Minor changes
    Some documentation changes and code changes

--- R/weight.R
+++ R/weight.R
@@ -3,7 +3,8 @@
 #' This function computes the weight used for correlation using timepoints used and lags used
 #'
 #' @param t a vector of timepoints
-#' @param k a integer value of lag used
+#' @param lag a integer value of lag used
+#' @param C a numeric of the constant used in penalty and weight of correlation composed of gaussian kernel
 #' @return a list containing w0 and vector w used in computing weighted correlation
 #'
 #' @example weight(t = c(0, 5, 10, 15, 20), lag = 1, C = 20)
--- tests/testthat/test_comp_corr.R
+++ tests/testthat/test_comp_corr.R
@@ -1,7 +1,7 @@
 
 context("")
-test_that("the best lag is", {
+test_that("the weighted corr is", {
   expect_equal( as.numeric(comp.corr(data = rbind(0:5, 1:6), timepoints = c(0, 5, 10, 15, 20, 25), C = 10)), 1)
-  expect_equal( as.numeric(comp.corr(data = rbind(c(NA, 1:5), 1:6), timepoints = c(0, 5, 10, 15, 20, 25), C = 10)), 0.98017)
+  expect_equal( as.numeric(comp.corr(data = rbind(c(1:5, NA), 1:6), timepoints = c(0, 5, 10, 15, 20, 25), C = 10)), 0.082085)
   expect_error( as.numeric(comp.corr(data = rbind(0:5, 1:6), timepoints = c(0, 5, 10, 15, 20, 25, 27), C = 10)))
 })
