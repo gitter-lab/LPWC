deabd0e7fc0f104795012484e41e7a449c1e2b5a
Thevaa Chandereng <chandereng@wisc.edu>
2017-11-02 02:19:26 -0500

    Comp corr changes
    Modified comp corr function. Made it simpler to solve it quickly.

--- R/comp_corr.R
+++ R/comp_corr.R
@@ -2,12 +2,12 @@
 #'
 #' This function computes the correlation upon picking the best lag and adjusting the data for lags
 #'
-#' @param data a matrix of data with the columns representing different timepoints
-#' @param timepoints a vector which contains all the timepoints represented in the data
+#' @param data a matrix of data with the columns representing different timepoints (NA's added when lags are needed)
+#' @param time a vector which contains all the timepoints represented in the data
 #' @param C a numeric value of C used in computing weighted correlation
 #' @return a simmilarity matrix with values between -1 and 1 (1 highly correlated, 0 no correlation )
 #'
-#' @example comp.corr(t(array(c(rnorm(16), NA, NA, rnorm(6), rnorm(7), NA, rnorm(8)), c(8, 5))), timepoints = c(0, 5, 10, 15, 20, 25, 30, 35), C = 10)
+#' @example comp.corr(array(rnorm(40), c(5, 8)), time = c(0, 5, 10, 15, 20, 25, 30, 35), C = 10, lags = c(0, 1, -1, 1, 2))
 #'
 #'
 #' @author Thevaa Chandereng, Anthony Gitter
@@ -15,33 +15,18 @@
 #'
 
 
-comp.corr <- function(data, timepoints, C){
-  stopifnot(dim(data)[2] == length(timepoints), all(is.numeric(timepoints)),
-            is.numeric(C))
+comp.corr <- function(data, time, C){
+  stopifnot(all(dim(data) == dim(time)), is.numeric(C))
   corr <- array(NA, c(dim(data)[1], dim(data)[1]))
   for(j in 1:(dim(data)[1] - 1)){
     for(i in (j + 1):dim(data)[1]){
-      pair <- rbind(data[i, ], data[j, ])
-      if(all(apply(pair, 2, function(x){all(!is.na(x))}))){
-        corr[i, j] <- cor(pair[1, ], pair[2, ])
-      }
-      else{
-        t1 <- timepoints[complete.cases(pair[1, ])]
-        t2 <- timepoints[complete.cases(pair[2, ])]
-        time <- weight.lag(t1, t2)
-        comp <- complete.cases(t(pair))
-        time <- time[, 1:sum(comp)]
-        pair.full <- pair[, comp]
-        if(all(time[1,] == time[2,])){
-          corr[i, j] <- cor(pair.full[1, ], pair.full[2, ])
-        }
-        else{
-          weights <- apply(time, 2, function(x){(diff(x))^2})
-          corr[i, j] <- exp(- 1 / C * mean(weights)) *
-            wt.corr(pair.full[1, ], pair.full[2, ], exp(-1 / C * weights))
-        }
-      }
+      pair <- weight.lag(data[i, !is.na(data[i, ])], data[j, !is.na(data[j, ])])
+      times <- weight.lag(time[i, !is.na(time[i, ])], time[j, !is.na(time[j, ])])
+      weights <- apply(times, 2, function(x){(diff(x)) ^ 2})
+      corr[i, j] <- exp(-1 / C * mean(weights)) * wt.corr(pair[1, ], pair[2, ], exp(-1 / C * weights))
     }
   }
   return(as.dist(corr))
 }
+
+
