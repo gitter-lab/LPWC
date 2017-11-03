6bbef3b795642b56f6c4646ddace5555f82e1873
Thevaa Chandereng <chandereng@wisc.edu>
2017-11-03 16:08:19 -0500

    Minor changes to findC
    adding another condition

--- R/findC.R
+++ R/findC.R
@@ -1,18 +1,18 @@
 #' Finding best C
-#'  
+#'
 #' This function computes the best C using the timepoints and max lag in the dataset
-#' 
+#'
 #' @param timepoints a vector of timepoints used in the dataset
-#' @param max.lag a numeric value with maximum lags allowed 
+#' @param max.lag a numeric value with maximum lags allowed
 #' @param pi a numeric value between 0.5 and 1 for the upper bound on the penalty
-#' @param iter a numeric value with number of penalty 
+#' @param iter a numeric value with number of penalty
 #' @return a vector of length (10 or indicated by iter) different C to be tested
 #'
 #' @example findC(c(0, 5, 10, 15, 20, 25), max.lag = 1, iter = 15)
-#' 
-#' 
+#'
+#'
 #' @author Thevaa Chandereng, Anthony Gitter
-#' 
+#'
 #'
 
 
@@ -21,7 +21,7 @@ findC <- function(timepoints, max.lag = NULL, pi = 0.95, iter = 10){
   if(is.null(max.lag)){
     max.lag <- floor(length(timepoints) / 4)
   }
-  stopifnot(all(is.numeric(timepoints)), max.lag <= length(timepoints) / 4, is.numeric(max.lag))
+  stopifnot(all(is.numeric(timepoints)), max.lag <= length(timepoints) / 4, max.lag %% 1 == 0)
   penalty <- seq(0.5, 0.95, length.out = iter)
   vals <- NULL
   for(i in 1:max.lag){
