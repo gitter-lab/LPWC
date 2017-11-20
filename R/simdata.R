92d6c6d72f443954b7a1012e9ea8e4abc73c8f42
Thevaa Chandereng <chandereng@wisc.edu>
2017-11-20 15:36:38 -0600

    Adding data
    Adding data and the file explaining the data

---
+++ R/simdata.R
@@ -0,0 +1,13 @@
+#' Example datasets for LPC
+#'
+#' Data is simulated from ImpulseDE function with 8 timepoints c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72)
+#'
+#' @docType data
+#'
+#' @format data matrix
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(simdata)
+"simdata"
