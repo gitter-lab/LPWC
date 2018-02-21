099d56875f7ebfd26a22316f67170e66ca505626
Thevaa Chandereng <chandereng@wisc.edu>
2018-02-21 15:52:16 -0600

    minor mistake fix

--- tests/testthat/test_prep_data.R
+++ tests/testthat/test_prep_data.R
@@ -5,7 +5,7 @@ test_that("the prep data is", {
   expect_equal(as.numeric(prep.data(data = array(c(3, 6, 7, 7, 9), c(1, 5)), lags = -1, timepoints = c(0, 5, 10, 15, 20))$data),
                c(6, 7, 7, 9 , NA))
   expect_equal(as.numeric(prep.data(data = array(c(3, 6, 7, 7, 9), c(1, 5)), lags = -1, timepoints = c(0, 5, 10, 15, 20))$time),
-               c(5, 10, 15, 20, NA))
+               c(0, 5, 10, 15, NA))
   expect_equal(as.numeric(prep.data(data = array(c(0, 5, 6, 7, 9), c(1, 5)), lags = 0, timepoints = c(0, 5, 10, 15, 20))$data), c(0, 5, 6, 7, 9))
   expect_error(prep.data(data = array(c(0, 5, 6, 7, 9), c(1, 5)), timepoints = c(0, 5, 10, 15, 20), lags = 10))
   expect_error(prep.data(data = array(c(0, 5, 6, 7, 9), c(1, 5)), timepoints = c(0, 5, 10, 15, 20), lags = "a"))
