9b6826f6938043c6c9b784742db67a941b01b0fd
Anthony Gitter <gitter@biostat.wisc.edu>
2018-04-04 09:47:01 -0500

    Add ImpulseDE reference

--- R/simdata.R
+++ R/simdata.R
@@ -1,6 +1,7 @@
 #' Example datasets for LPC
 #'
 #' Data is simulated from ImpulseDE function with 8 timepoints c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72)
+#' See https://doi.org/10.1093/bioinformatics/btw665
 #'
 #' @docType data
 #'
