4550dcc8151fca2edbf3019b34840af149c56c1d
Thevaa Chandereng <chandereng@wisc.edu>
2018-06-09 16:23:46 -0500

    FIXED BUG

--- R/prep_data.R
+++ R/prep_data.R
@@ -13,7 +13,7 @@
 #' @examples
 #' prep.data(array(rnorm(20), c(5, 4)), c(0, 0, 0, -1, 1),
 #'           timepoints = c(0, 5, 15, 30))
-#' prep.data(array(runif(100, 0, 10), c(10, 10)), sample((-2:2), size = 10, replace = T),
+#' prep.data(array(runif(100, 0, 10), c(10, 10)), sample((-2:2), size = 10, replace = TRUE),
 #'           timepoints = c(0, 5, 15, 30, 45, 60, 75, 80, 100, 120))
 #'
 #' @author Thevaa Chandereng, Anthony Gitter
--- R/score.R
+++ R/score.R
@@ -8,7 +8,7 @@
 #'
 #' @examples
 #' score(runif(10, 0, 1), c(2, 0, 0, 0, 3, 2, -1, 2, 0, 1))
-#' score(runif(20, 0.5, 0.8), sample(-3:3, size = 20, replace = T))
+#' score(runif(20, 0.5, 0.8), sample(-3:3, size = 20, replace = TRUE))
 #'
 #'
 #' @author Thevaa Chandereng, Anthony Gitter
