bc2935db456c4c0cf1a099b9c14e3528da731d68
Thevaa Chandereng <chandereng@wisc.edu>
2018-06-12 22:49:52 -0500

    added longer series

--- R/findC.R
+++ R/findC.R
@@ -12,7 +12,7 @@
 #' @examples
 #' findC(c(0, 5, 10, 15, 20, 25), max.lag = 1, iter = 15)
 #' findC(c(2, 4, 8, 16, 32, 64, 128, 256), iter = 5)
-#' findC(c(1, 5, 8, 11, 13, 15), pi = 0.8, iter = 20)
+#' findC(c(2, 6, 10, 15, 22, 30, 40, 55, 80), pi = 0.8, iter = 20)
 #' findC(c(1, 2, 3.2, 4, 5.3, 7), pi = 0.99)
 #'
 #' @author Thevaa Chandereng, Anthony Gitter
