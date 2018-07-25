b01c5272c0345f4106afd254950759e190597d27
Thevaa Chandereng <chandereng@wisc.edu>
2018-07-25 00:10:53 -0500

    added testthat for findC pi

--- tests/testthat/test_findC.R
+++ tests/testthat/test_findC.R
@@ -4,4 +4,6 @@ test_that("The optimal C is/are", {
   expect_equal(round(findC(timepoints = c(0, 5, 10, 15, 20, 25, 30, 35), max.lag = 2)[4], 2), 132.97)
   expect_equal(round(findC(timepoints = c(0, 5, 15, 30, 60, 75, 90, 120), max.lag = 2)[5], 2), 2212.09)
   expect_error(findC(timepoints = c(0, 5, 10, 15, 20, 25, 30, 35), max.lag = 3))
+  expect_equal(findC(timepoints = c(0, 5, 15, 30, 60, 75, 90, 120), max.lag = 2, pi = 0.2))
+  expect_equal(findC(timepoints = c(0, 5, 15, 30, 60, 75, 90, 120), max.lag = 2, pi = 1.12))
 })
