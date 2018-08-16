efdd13ab1161c2a7ad2c9e6a0a546d55f43c28f9
Thevaa Chandereng <chandereng@wisc.edu>
2018-08-16 01:01:14 -0500

    added test cases

--- tests/testthat/test_findC.R
+++ tests/testthat/test_findC.R
@@ -1,5 +1,6 @@
 context("")
 test_that("The optimal C is/are", {
+    expect_equal(round(findC(timepoints = c(0, 5, 15, 30, 60, 75, 90, 120))[1], 2), 1065.97)
   expect_equal(round(findC(timepoints = c(0, 5, 15, 30, 60, 75, 90, 120), max.lag = 2)[1], 2), 1065.97)
   expect_equal(round(findC(timepoints = c(0, 5, 10, 15, 20, 25, 30, 35), max.lag = 2)[4], 2), 132.97)
   expect_equal(round(findC(timepoints = c(0, 5, 15, 30, 60, 75, 90, 120), max.lag = 2)[5], 2), 2212.09)
--- tests/testthat/test_wt_corr.R
+++ tests/testthat/test_wt_corr.R
@@ -1,5 +1,6 @@
 context("")
 test_that("the weighted correlation is", {
+  expect_equal(wt.corr(1:6, 2:7, rep(0, 6)), 1)
   expect_equal(wt.corr(1:6, 2:7, c(2, 1, 2, 2, 1, 1)), 1)
   expect_equal(round(wt.corr(c(1, 2, -9, 4, 5), c(2:6), c(0.5, 1, 2, 0.5, 2)), 2), 0.42)
   expect_error(wt.corr(1:6, 2:7, c(2, 1, 2, 2, 1, 1, 6)))
