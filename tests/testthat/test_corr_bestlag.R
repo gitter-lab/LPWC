50359997243327975f4752885d3ab9c9f326c042
Thevaa Chandereng <chandereng@wisc.edu>
2018-10-26 10:49:48 -0500

    added test cases

--- tests/testthat/test_corr_bestlag.R
+++ tests/testthat/test_corr_bestlag.R
@@ -12,8 +12,10 @@ test_that("the corr is", {
                timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72), penalty = "high")$lags[1]), 1)
   expect_equal(signif(corr.bestlag(data = simdata[20:30, ],
                timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72))$corr[5], 2), 0.91)
-  expect_error(signif(corr.bestlag(data = simdata,
-               timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72, 75))$corr[10], 2))
+  expect_equal(signif(corr.bestlag(data = simdata[20:30, ],
+                                   timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72))$corr[5], 2), 0.91)
+  expect_equal(signif(corr.bestlag(data = simdata[1:10, ],
+               timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72), penalty = "low")$corr[10], 2), 0.91)
   expect_error(signif(corr.bestlag(data = simdata,
                timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72), max.lag = 4)$corr[10], 2))
   expect_error(corr.bestlag(data = simdata, timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72),
