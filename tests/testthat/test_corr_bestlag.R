3246717dbbb79b5bef69abfb0054d82caa17c2af
Thevaa Chandereng <chandereng@wisc.edu>
2018-10-28 11:47:46 -0500

    added removed test case

--- tests/testthat/test_corr_bestlag.R
+++ tests/testthat/test_corr_bestlag.R
@@ -18,6 +18,7 @@ test_that("the corr is", {
                timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72), penalty = "low")$corr[10], 2), 0.91)
   expect_error(signif(corr.bestlag(data = simdata,
                timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72), max.lag = 4)$corr[10], 2))
+  expect_error(corr.bestlag(data = simdata, timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72, 75)))
   expect_error(corr.bestlag(data = simdata, timepoints = c(0, 2, 4, 6, 8, 18, 24, 32, 48, 72),
                             max.lag = 2, C = -1))
   expect_error(corr.bestlag(data = simdata, timepoints = c(0, 1, 2, 4, 6, 8, 12, 16, 20, 25),
