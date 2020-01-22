371525af5c71a5b6c2bc0e804e9a86b23bd022f9
Thevaa Chandereng <chandereng@wisc.edu>
2020-01-22 12:32:53 -0600

    added test case that failed in the earlier findC version

--- tests/testthat/test_findC.R
+++ tests/testthat/test_findC.R
@@ -4,6 +4,7 @@ test_that("The optimal C is/are", {
   expect_equal(round(findC(timepoints = c(0, 5, 15, 30, 60, 75, 90, 120), max.lag = 2)[1], 2), 1065.97)
   expect_equal(round(findC(timepoints = c(0, 5, 10, 15, 20, 25, 30, 35), max.lag = 2)[4], 2), 132.97)
   expect_equal(round(findC(timepoints = c(0, 5, 15, 30, 60, 75, 90, 120), max.lag = 2)[5], 2), 2212.09)
+  expect_equal(round(findC(timepoints = c(0, 0.5, 3, 6, 12), max.lag = 1)[1], 0), 19)
   expect_error(findC(timepoints = c(0, 5, 10, 15, 20, 25, 30, 35), max.lag = 3))
   expect_error(findC(timepoints = c(0, 5, 15, 30, 60, 75, 90, 120), max.lag = 2, pi = 0.2))
   expect_error(findC(timepoints = c(0, 5, 15, 30, 60, 75, 90, 120), max.lag = 1, pi = 1.12))
